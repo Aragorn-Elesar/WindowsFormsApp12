f67a2fc5b5b4ae95403c0de6746fa6beb69dfd31
Gord_oe65 <Gord_oe65@student.itstep.org>
2023-04-23 13:47:30 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "All Files(*.*)|*.*|Text Files (*.txt) | *.txt || ";
+            open.FilterIndex = 1;
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader reader = File.OpenText(open.FileName);
+                textBox1.Text = reader.ReadToEnd(); 
+                reader.Close(); 
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string g = textBox1.Text;
+            Form2 form2 = new Form2(g);
+            form2.Show();
+            g = textBox1.Text;
+            // сделать повторное откритие файла после сохранения во второй форме 
+        }
+    }
+}
---
+++ WindowsFormsApp12/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string s)
+        {
+            InitializeComponent();
+            textBox1.Text = s;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer =
+                new StreamWriter(save.FileName);
+                writer.Write(textBox1.Text);
+                writer.Close();
+            }
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult= DialogResult.Cancel;
+        }
+
+        
+    }
+}
